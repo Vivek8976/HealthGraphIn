b6ca993c6ecb1a2116cca6980d5f83f8f3f0a9bf
Vivek8976 <vivekg913@gmail.com>
2015-11-02 04:12:18 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/vivek/healthgraphin/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.vivek.healthgraphin;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+public void onClick(View view){
+    Intent i = new Intent(this, ProfileActivity.class);
+    startActivity(i);
+}
+
+}
